9409cb5bf36b4daa5531891b8c46a4562ab95327
Himanshu Sajwan <himanshusajwan001@gmail.com>
2021-01-03 00:35:27 +0530

    mobile handling

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,22 +3,22 @@ import './Footer.css';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import Fontawesome from '../../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js'
 function Footer() {
-    var toll_free_number = 18002001234;
-    var facebook_link = "www.facebook.com/cripumps";
-    var website = "www.crigourps.com";
+    // var toll_free_number = 18002001234;
+    // var facebook_link = "";
+    // var website = "";
     return (
         <div class="footer">
             <div id="toll__free" >
             <i class="fas fa-phone-alt"></i>
-                Toll free {toll_free_number}
+                <p class="footer__p">Toll free 18002001234</p>
             </div>
             <div id="facebook">
-            <i class="fab fa-facebook"></i>
-                {facebook_link}
+                <i class="fab fa-facebook"></i>
+                <p class="footer__p">www.facebook.com/cripumps</p>
             </div>
             <div id="website">
             <i class="fas fa-globe"></i>
-                {website}
+            <p class="footer__p">www.crigourps.com</p>
             </div>
         </div>
     )
--- src/components/Products.js
+++ src/components/Products.js
@@ -5,16 +5,15 @@ function Products() {
     return (
         <>
             <div className="product__container">
-                <p>INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
+                <p class="product__p">INSTALLED OVER 10 LAKHS STAR RATED PUMPSETS ACROSS THE COUNTRY RESULTING IN A CUMULATIVE SAVING OF MORE THAN 9,000 MILLION UNITS OF POWER FOR THE NATION.</p>
                 <figure>
                     <img src={photo3} id = "photo3" alt="products"></img>
                     <figcaption> Valves - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</figcaption>
                 </figure>
                 <hr class="solid"></hr>
-                <div id="segments">
+                <div class="segments">
                     <h5>C.R.I. FLUID SYSTEMS PRODUCTS CATER TO DIVERSE SEGMENTS</h5>
-                    <p>CHEMICALS & PROCESS | POWER | WATER & WASTE WATER | OILS & GAS | PHARMA | SUGARS & DISTILLERIES | PAPER & PULP | MARINE & DEFENCE | METAL & MINING | FOOD & BEVERAGE |</p>
-                    <p>PETROCHEMICAL & REFINERIES | SOLAR | BUILDING | HVAC | FIRE FIGHTING | AGRICULTURE & RESIDENTIAL</p>
+                    <p class="product__p">CHEMICALS & PROCESS | POWER | WATER & WASTE WATER | OILS & GAS | PHARMA | SUGARS & DISTILLERIES | PAPER & PULP | MARINE & DEFENCE | METAL & MINING | FOOD & BEVERAGE |PETROCHEMICAL & REFINERIES | SOLAR | BUILDING | HVAC | FIRE FIGHTING | AGRICULTURE & RESIDENTIAL</p>
                 </div>
             </div>
         </>
