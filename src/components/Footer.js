a1a49fcc47b660f13432888e01c4f670fbd02e05
Himanshu Sajwan <himanshusajwan001@gmail.com>
2021-01-03 14:55:40 +0530

    final commit

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -3,22 +3,20 @@ import './Footer.css';
 // import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 // import Fontawesome from '../../node_modules/@fortawesome/fontawesome-free/js/fontawesome.js'
 function Footer() {
-    // var toll_free_number = 18002001234;
-    // var facebook_link = "";
-    // var website = "";
+
     return (
         <div class="footer">
             <div id="toll__free" >
             <i class="fas fa-phone-alt"></i>
-                <p class="footer__p">Toll free 18002001234</p>
+            <p class="footer__p">Toll free 18002001234</p>
             </div>
             <div id="facebook">
                 <i class="fab fa-facebook"></i>
-                <p class="footer__p">www.facebook.com/cripumps</p>
+                <a href="www.facebook.com/cripumps" id="fb__link" class="footer__p">www.facebook.com/cripumps</a>
             </div>
             <div id="website">
             <i class="fas fa-globe"></i>
-            <p class="footer__p">www.crigourps.com</p>
+            <a href="www.crigourps.com"  class="footer__p" id ="site__link">www.crigourps.com</a>
             </div>
         </div>
     )
